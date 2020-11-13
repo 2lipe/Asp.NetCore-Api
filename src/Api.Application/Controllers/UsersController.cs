26d1f9e1378f01f1864b71af5d9db0fd6b01b754
Felipe <felipe.svfx@gmail.com>
2020-11-12 23:00:57 -0300

    implementing Get and Post methods on API

--- src/Api.Application/Controllers/UsersController.cs
+++ src/Api.Application/Controllers/UsersController.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Net;
 using System.Threading.Tasks;
+using Api.Domain.Entities;
 using Api.Domain.Interfaces.Services.User;
 using Microsoft.AspNetCore.Mvc;
 
@@ -20,7 +21,8 @@ namespace Api.Application.Controllers
     [HttpGet]
     public async Task<ActionResult> GetAll()
     {
-      if (!ModelState.IsValid) return BadRequest(ModelState);
+      var isModelStateInvalid = !ModelState.IsValid;
+      if (isModelStateInvalid) return BadRequest(ModelState);
 
       try
       {
@@ -36,11 +38,41 @@ namespace Api.Application.Controllers
     [Route("{id}", Name = "GetWithId")]
     public async Task<ActionResult> Get(Guid id)
     {
-      if (!ModelState.IsValid) return BadRequest(ModelState);
+      var isModelStateInvalid = !ModelState.IsValid;
+      if (isModelStateInvalid) return BadRequest(ModelState);
 
       try
       {
         return Ok(await _service.Get(id));
+      }
+      catch (ArgumentException e)
+      {
+        return StatusCode((int)HttpStatusCode.InternalServerError, e.Message);
+      }
+    }
+
+    [HttpPost]
+    public async Task<ActionResult> Post([FromBody] UserEntity user)
+    {
+      var isModelStateInvalid = !ModelState.IsValid;
+      if (isModelStateInvalid)
+      {
+        return BadRequest(ModelState);
+      }
+
+      try
+      {
+        var result = await _service.Post(user);
+
+        if (result != null)
+        {
+          return Created(new Uri(Url.Link("GetWithId", new { id = result.Id })), result);
+        }
+        else
+        {
+          return BadRequest();
+        }
+
       }
       catch (ArgumentException e)
       {
